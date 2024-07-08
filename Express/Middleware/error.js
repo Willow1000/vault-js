e7edd276a43897c48b7486eaaf0c8f10807c9e2c
Tech-Savvy <wilkinsondari7@gmail.com>
2024-07-08 06:55:31 +0300

    Delete Express directory

--- Express/Middleware/error.js
+++
@@ -1,9 +0,0 @@
-const {logEvents} = require('./logs')
-
-
-const errorLog = (err,req,res,next)=>{
-    logEvents(`${err.name} ${err.message}`,'errors.txt')
-    next()
-}
-
-module.exports = {errorLog}
--- Express/Route/root.js
+++
@@ -1,9 +0,0 @@
-const {Router} = require("express")
-const path = require("path")
-const rout = Router()
-
-rout.get("^/$|index(.html)?",(req,res)=>{
-    res.status(200).sendFile(path.resolve('views','index.html'))
-})
-
-module.exports = rout
